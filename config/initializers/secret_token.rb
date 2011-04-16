4c2d5cbc719609159f78522e0caecfe183cef1a7
plukevdh <hungerandthirst@gmail.com>
2011-04-15 22:57:04 -0400

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Readabilify::Application.config.secret_token = 'd979918aa6aaa34b383782317a519ff8aadf3aa6effefda0e989b4f620a3b179e86acc5e29aa2394352060b4b972284d46d4fd22cdda6a9aab51f672e9136a40'
