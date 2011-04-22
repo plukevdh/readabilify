b1dd7be31665a8ccae8b351ae8f679acc01b4cbc
plukevdh <hungerandthirst@gmail.com>
2011-04-21 23:44:33 -0400

    styling + deauth

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,4 +5,9 @@ class HomeController < ApplicationController
     @bookmarks = readability :bookmarks, {favorite: params[:favorite] || 0, 
                                          archive: params[:archive] || 0} if readabilified?
   end
+
+  def destroy
+    reset_session
+    redirect_to root_path, notice: "You have logged out of Readability."
+  end
 end
