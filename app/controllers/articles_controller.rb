bc965e7e38721bc910d5388216491e16a456d62e
plukevdh <hungerandthirst@gmail.com>
2011-05-03 02:45:19 -0400

    yay pdf libs.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,17 @@
 class ArticlesController < ApplicationController
   def show
-    article = readability :articles, {id: params[:id]}
+    article = readability(:articles, {id: params[:id]}).to_options
 
-    output = Bookit::Article.new article
+    output = Bookit::Article.new({
+      content: article[:content],
+      author: article[:author] || "",
+      date_published: (article[:date_published] ? Time.parse(article[:date_published]) : Time.now).strftime("%m/%d/%Y"),
+      url: article[:url],
+      title: article[:title]
+    }, Bookit::Parser::Html)
 
     respond_to do |format|
-      format.pdf { send_file article.to_pdf }
+      format.pdf { send_data output.to_print.render, filename: "#{output.title}.pdf" }
     end
   end
 end
